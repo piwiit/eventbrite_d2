faf2bf270fa44b8e7c5becfd1fd7c6065ae7075e
piwiit <bouillaux@hotmail.fr>
2020-11-03 14:13:32 +0100

    add device

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: 'events#index'
   devise_for :users
-  resources :users, only: [:show]
-  resources :events
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: %i[show]
+  resources :events # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
