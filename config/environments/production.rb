7975a6d653a322a1c10d41ea96353c72dfa4d4e2
piwiit <bouillaux@hotmail.fr>
2020-11-03 14:52:42 +0100

    add device

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,6 +112,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.default_url_options = {
-    host: 'https://safe-oasis-88190.herokuapp.com/'
+    host: 'https://desolate-bayou-88355.herokuapp.com/'
   }
 end
