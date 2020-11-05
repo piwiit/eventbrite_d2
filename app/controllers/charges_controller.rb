55887a0c84a27f0145273d80b3464b8e27d5898b
piwiit <bouillaux@hotmail.fr>
2020-11-05 14:21:33 +0100

    add css

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,25 +1,46 @@
 class ChargesController < ApplicationController
-  def new; end
+  def new
+    @event = Event.find_by(id: params[:event_id])
+    @price = @event.price
+  end
 
   def create
-    @amount = 500
+    @event = Event.find_by(id: params[:event_id])
+    @price = @event.price
+    amount = @price * 100
 
-    customer =
-      Stripe::Customer.create(
-        { email: params[:stripeEmail], source: params[:stripeToken] }
-      )
+    if is_a?(@event)
+      Attendance.create(user_id: current_user.id, event_id: @event.id)
+      redirect_to event_path(@event.id)
+    else
+      customer =
+        Stripe::Customer.create(
+          { email: params[:stripeEmail], source: params[:stripeToken] }
+        )
 
-    charge =
-      Stripe::Charge.create(
-        {
-          customer: customer.id,
-          amount: @amount,
-          description: 'Rails Stripe customer',
-          currency: 'usd'
-        }
+      charge =
+        Stripe::Charge.create(
+          {
+            customer: customer.id,
+            amount: amount,
+            description: 'Rails Stripe customer',
+            currency: 'usd'
+          }
+        )
+    end
+
+    if customer.save && charge.save
+      Attendance.create(
+        event_id: @event.id,
+        user_id: current_user.id,
+        stripe_customer_id: customer.id
       )
+      redirect_to event_path(@event.id)
+    else
+      render 'new'
+    end
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
-  end # Amount in cents
+  end # Amount in cents # Amount in cents
 end
